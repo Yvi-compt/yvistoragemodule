043adee93aaf8f1f429257f4cc0531a87be77e7f
Yvi-compt <yvesplacide@gmail.com>
2025-02-19 21:16:00 -0500

    Update yvi_storage_mod.tf

--- yvi_storage_mod.tf
+++ yvi_storage_mod.tf
@@ -1,5 +1,5 @@
 module "storage_account" {
-  source = "./module_storage_account/"
+  source = "./yvi_module_storage/"
 
   storage_account_name  = local.storage_account_name
   resource_group_name   = local.resource_group_name

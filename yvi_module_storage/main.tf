920521904f833777ae509552a2c96798e551a46c
Yvi-compt <yvesplacide@gmail.com>
2025-02-19 21:11:38 -0500

    Update main.tf

--- yvi_module_storage/main.tf
+++ yvi_module_storage/main.tf
@@ -1,5 +1,5 @@
 #module for storage account
-resource "azurerm_storage_account" "storage_account" {
+resource "azurerm_storage_account" "yvi_storage_account" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
   location                 = var.location
